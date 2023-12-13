168b54e86a8584060083d6b479b5b5683db03133
karenhernandez <khernandez0329@gmail.com>
2023-12-12 17:29:09 -0800

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
 
-def greet(name):
-    pass
+def greet_programmer():
+    print("Hello, programmer!")
+
+
+def greet(Kara):
+    print(f"Hello, {Kara}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
     pass
+    num1 = 45
+    num2 = 55
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2.0
